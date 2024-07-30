fb98a8e20d6e90db1970a7bf5c942d1794323969
Alice Huang <alicehccn@gmail.com>
2024-07-30 15:18:47 -0700

    added some func

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
-import { Matrix } from "./matrix";
-
+import { Matrix, Move } from "./matrix";
 
 
 
 function main () {
   const matrix = new Matrix(9);
   matrix.initalize();
-  matrix.print()
-  matrix.rotate90()
+  matrix.makeMove(new Move(0, 5, 4))
+  matrix.makeMove(new Move(1, 4, 2))
+  matrix.makeMove(new Move(0, 7, 7))
+  matrix.makeMove(new Move(1, 7, 3))
   matrix.print()
 }
 
--- matrix.ts
+++ matrix.ts
@@ -1,3 +1,21 @@
+export class Move {
+  player: number;
+  x: number;
+  y: number;
+  captured?: [];
+
+  constructor(player, x, y) {
+    this.player = player;
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const players = {
+  0: '⚫',
+  1: '⚪',
+}
+
 
 export class Matrix {
   size: number;
@@ -9,37 +27,43 @@ export class Matrix {
   }
 
   initalize () {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 1; i < this.size+1; i++) {
       const row: string[] = []
-      for (let j = 0; j < this.size; j++) {
-        const index = i+j;
-        const value = String.fromCharCode(index + 65);
-        row.push(value);
+      for (let j = 0; j < this.size+1; j++) {
+        const index = i + '' + String.fromCharCode(j + 65);
+        row.push(index);
       }
       this.rows.push(row)
     }
   }
 
-  getPosition (x: number, y: string) {
-    return this.rows[x][y]
+  getPosition (x: number, y: number) {
+    return this.rows[x-1][y]
+  }
+
+  makeMove (move: Move) {
+    this.rows[move.x-1][move.y] = players[move.player]
   }
 
   print () {
     for (let i = 0; i < this.size; i++) {
-      process.stdout.write(JSON.stringify(this.rows[i]) + '\n')
+      process.stdout.write(JSON.stringify(this.rows[i]) + '\n\n')
     }
-    process.stdout.write(' ===================================\n')
   }
 
   rotate90 () {
     const tempRows: string[][] = [];
     for (let i = this.size - 1; i >= 0; i--) {
       const tempRow: string[] = []
-      for (let j = this.size - 1; j >= 0; j--) {
+      for (let j = this.size; j >= 0; j--) {
         tempRow.push(this.rows[i][j])
       }
       tempRows.push(tempRow);
     }
     this.rows = tempRows;
+  }
+
+  stringToNum (char: string): number {
+    return char.toUpperCase().charCodeAt(0) - 65;
   }
 }
