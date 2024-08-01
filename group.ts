4b54f7bf31b13d5057d881cc4f68fc3bde4945a5
Alice Huang <alicehccn@gmail.com>
2024-08-01 12:48:10 -0700

    add files

---
+++ group.ts
@@ -0,0 +1,21 @@
+export class Group {
+  group: string[];
+
+  
+  public addToGroups(move: string) {
+    // this.groups.forEach((g) => {
+
+    // })
+
+    
+  }
+
+  public foundAjacent(item: string) {
+    for (let i = 0; i < this.group.length; i++) {
+      if(this.group.includes(item)) {
+        
+      }
+      
+    }
+  }
+}
